7dc9d7a3f7e2b86181d9635b910a47237577673b
PolinaMoiseyenko <pmoiseyenko@gmail.com>
2018-10-01 01:08:59 +0300

    additional-5

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let strArray = str.split("");
-  let length = bracketsConfig.length;
-  let openBrackets = [],
+  const strArray = str.split("");
+  const length = bracketsConfig.length;
+  const openBrackets = [],
       closeBrackets = [];
   let i = 0, j = 0, count = 0;
+  let stack = [];
 
   for (i = 0; i < length; i++) {
     openBrackets[i] = bracketsConfig[i][0];
     closeBrackets[i] = bracketsConfig[i][1];
   }
   for (i = 0; i < strArray.length; i++) {  
-    if (strArray[0] === openBrackets[i]) {
+    //if (strArray[0] === openBrackets[i]) {
       for (i = 0; i < strArray.length; i++) {
         for (j = 0; j < openBrackets.length; j++){
-          if(strArray[i] === openBrackets[j]) {
+          if (openBrackets[j] === closeBrackets[j] && strArray[i] == closeBrackets[j]) {
+            if (strArray[i] === closeBrackets[stack[stack.length - 1]]) {
+                stack.pop();
+                count--;
+            } 
+            else {
+              stack.push(j);
+              count++;
+            }
+          } else if (strArray[i] === openBrackets[j]) {
+            stack.push(j);
             count++;
+          } else if (strArray[i] === closeBrackets[j]) {
+            if (j == stack.pop()) {
+              count--;
+             } else return false;
           }
-          if(strArray[i] === closeBrackets[j]) {
-            count--;
-          }
-          if (count < 0)
-            return false;
         }
-      } 
-      if (count === 0) {
-        return true;       
-      } else
-        return false;
-    } else if (strArray[0] === closeBrackets[i])
-        return false; 
+        if (count < 0)
+            return false;
+      }
+      if (count === 0)
+        return true;
+        else
+          return false;
   }
 }
